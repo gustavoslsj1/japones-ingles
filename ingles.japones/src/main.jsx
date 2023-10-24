ca0d075b0819f68443fcd606b60bba5db83b6173
gustavo lima sales junior <102685940+gustavoslsj1@users.noreply.github.com>
2023-10-24 03:38:32 -0300

    terminando a base do site

--- ingles.japones/src/main.jsx
+++ ingles.japones/src/main.jsx
@@ -3,8 +3,37 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+import{ createBrowserRouter, RouterProvider ,Navigate} from "react-router-dom";
+
+import Home from './routes/Home.jsx'
+import Japones from './routes/Japones.jsx';
+import ErrorPage from './routes/errorPage.jsx';
+import Ingles from './routes/Ingles.jsx';
+const router = createBrowserRouter ([
+  {
+    path:"/",
+    element:<App/>,
+    errorElement: <ErrorPage/>,
+    children: [
+      {
+        path:"/",
+        element:<Home/>,
+      },
+      {
+        path:"japones",
+        element:<Japones/>,
+      },
+      {
+        path:"ingles",
+        element:<Ingles/> 
+      }
+    ],
+  },
+]);
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
