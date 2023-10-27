97317135571638e58387be53776e45b3aa95608a
gustavo lima sales junior <102685940+gustavoslsj1@users.noreply.github.com>
2023-10-27 03:15:24 -0300

    adicionando funçoes no file japones

--- ingles.japones/src/routes/Home.jsx
+++ ingles.japones/src/routes/Home.jsx
@@ -2,12 +2,10 @@ import React from "react";
 import styles from '../css/Home.module.css';
 const Home = () => {
     return (
-        <>
-        
         <div>
             <p>Home</p>
         </div>
-        </>
+
     )
 }
 
--- ingles.japones/src/routes/Japones.jsx
+++ ingles.japones/src/routes/Japones.jsx
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from '../css/Japones.module.css'
 
 const Japones = ()=> {
+    const [showElement, setShowELement] = useState(false)
+    const [showButton , setShowButton] = useState(true)
+    function handleclick(){
+        console.log('clikado')
+        setShowELement(true)
+        setInterval(1000)
+        setShowButton(false)
+    }
     return(
         <div>
-            <h1>Japones</h1>
+            <div>
+                <h1>Japnones</h1>
+            </div>
+            <div>
+                {showButton ?<button onClick={handleclick} className={styles.start}> iniciar</button> :null}
+               {showElement? <input type="text" className={styles.caixa1} /> :null}
+            </div>
         </div>
+        
     )
 }
 
