3545705783d1a5f86804122e13738c7937597861
gustavo lima sales junior <102685940+gustavoslsj1@users.noreply.github.com>
2023-10-28 15:14:58 -0300

    colocando imagem

--- ingles.japones/src/routes/Japones.jsx
+++ ingles.japones/src/routes/Japones.jsx
@@ -1,9 +1,11 @@
 import React, { useState } from "react";
 import styles from '../css/Japones.module.css'
+import katana from '../img/Katana.jpg'
 
 const Japones = ()=> {
     const [showElement, setShowELement] = useState(false)
     const [showButton , setShowButton] = useState(true)
+    const [showImagem , setShowImagem] = useState(false)
     const [num,setNum] = useState(0)
     const randomNum = (min, max) => {
         return Math.floor(Math.random() *(max - min  + 1)) + min;
@@ -11,12 +13,13 @@ const Japones = ()=> {
     function handleclick(){
         setShowELement(true)
         setShowButton(false)
+        setShowImagem(true)
         setNum(randomNum(1,20));
         
         if (num == 1){
-            <h1> oi</h1>
+            console.log('oi')
         }else{
-            <h1>ola</h1>
+            console.log('ola')
         }
     }
     return(
@@ -27,6 +30,7 @@ const Japones = ()=> {
             <div>
                 {showButton ?<button onClick={handleclick} className={styles.start}> iniciar</button> :null}
                 {showElement? <input type="text" className={styles.caixa1} /> :null}
+                {showImagem?<img src={katana}></img>:null}
             </div>
         </div>
         
